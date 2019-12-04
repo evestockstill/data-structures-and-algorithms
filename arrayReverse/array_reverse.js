82e041323288f0bf96103e0367cf60be075a1d2f
Eve Stockstill <evestockstillpdx@gmail.com>
2019-12-03 19:30:38 -0800

    setup

--- arrayReverse/array_reverse.js
+++ arrayReverse/array_reverse.js
@@ -1,5 +1,4 @@
 const arrOne = [1, 2, 3, 4, 5, 6];
-// const arrTwo = [89, 2354, 3546, 23, 10, -923, 823, -12];
 const arrayReverser = arr => {
   let reversedArray = [];
   for(let i = arr.length - 1; i >= 0; i--) {
@@ -8,7 +7,5 @@ const arrayReverser = arr => {
   return reversedArray;
 };
 const backwardsArray = arrayReverser(arrOne);
-
-console.log(backwardsArray);
 
 module.exports = { arrayReverser };
