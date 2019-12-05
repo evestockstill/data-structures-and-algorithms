5e267775ac4b8e980f788329ec0922f4931c8fc1
Eve Stockstill <evestockstillpdx@gmail.com>
2019-12-05 12:38:17 -0800

    setup whiteboard

---
+++ whiteboard/whiteboard.js
@@ -0,0 +1,15 @@
+// Familiarize yourself with the grading rubric, so you know how to score the interview.
+
+// Look for effective problem solving, efficient use of time, and effective communication with the whiteboard space available.
+
+// Every solution might look a little different, but the candidate should be able to at least convince you that their code works to solve the problem.
+
+// Assign points for each item on the Rubric, according to how well the candidate executed on that skill.
+
+// Add up all the points at the end, and record the total at the bottom of the page.
+
+//   Example
+// Input	Output
+// [[1, 2, 3], [3, 5, 7], [1, 7, 10]][6, 15, 18]
+// [[0, 1, 5], [-4, 7, 2], [-3, 12, 11]][6, 5, 20]
+// The candidate should draw the input and output as a square of integers.
