6e8f422852dd5fd71ee36f90f836bde71eca3cba
Eve Stockstill <evestockstillpdx@gmail.com>
2019-12-08 18:48:24 -0800

    linked-list

--- linked-list/linkedList.js
+++ linked-list/linkedList.js
@@ -14,7 +14,9 @@
 //     Features
 // Create a Node class that has properties for the value stored in the Node, and a pointer to the next Node.
 // Within your LinkedList class, include a head property.Upon instantiation, an empty Linked List should be created.
+
 // Define a method called insert which takes any value as an argument and adds a new node with that value to the head of the list with an O(1) Time performance.
+
 // Define a method called includes which takes any value as an argument and returns a boolean result depending on whether that value exists as a Node’s value somewhere within the list.
 // Define a method called toString(or __str__ in Python) which takes in no arguments and returns a string representing all the values in the Linked List.
 // Any exceptions or errors that come from your code should be semantic, capturable errors.For example, rather than a default error thrown by your language, your code should raise /throw a custom, semantic error that describes what went wrong in calling the methods you wrote for this lab.
@@ -32,3 +34,24 @@
 // Will return false when searching for a value in the linked list that does not exist
 // Can properly return a collection of all the values that exist in the linked list
 // Ensure your tests are passing before you submit your solution.
+
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+class LinkedList {
+  head
+  insert(val) {
+    if (this.head === null) {
+      this.head = new Node(val);
+    }
+    else {
+      let n = new Node(val);
+      n.next = this.head;
+      this.head = n
+    }
+  }
+}
